01a10f0fafe17c80ac2e409548d93bce004b03b5
fran-byte <fran-byte@hotmail.com>
2025-04-07 19:45:22 +0200

    update parse cardinals and paht cardinals

--- src/parse/extract_map.c
+++ src/parse/extract_map.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 19:40:40 by frromero          #+#    #+#             */
-/*   Updated: 2025/04/07 17:03:27 by frromero         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:05:24 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 static void parse_orientation(t_game *data)
 {
-	char **orient;
 	int i;
 	int j;
 	int valid;
 	const char *prefixes[] = {"NO", "SO", "WE", "EA"};
 
 	i = 0;
-	orient = data->map.map_file;
 	valid = 1;
 	while (i < 4 && valid)
 	{
@@ -29,7 +27,7 @@ static void parse_orientation(t_game *data)
 		valid = 0;
 		while (++j < 4 && !valid)
 		{
-			if (orient[i][0] == prefixes[j][0] && orient[i][1] == prefixes[j][1] && (orient[i][2] == ' ' || orient[i][2] == '\t'))
+			if (data->map.map_file[i][0] == prefixes[j][0] && data->map.map_file[i][1] == prefixes[j][1] && (data->map.map_file[i][2] == ' ' || data->map.map_file[i][2] == '\t'))
 				valid = 1;
 		}
 		if (!valid)
