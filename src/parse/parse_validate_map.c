d5401ec0bef73702a275f996d903be76e8634760
fran-byte <fran-byte@hotmail.com>
2025-04-12 10:17:40 +0200

    update SPACES into the map (parse_validate_map.c)

--- src/parse/parse_validate_map.c
+++ src/parse/parse_validate_map.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 08:43:36 by frromero          #+#    #+#             */
-/*   Updated: 2025/04/10 16:49:53 by frromero         ###   ########.fr       */
+/*   Updated: 2025/04/12 10:12:45 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ static void find_player_position(t_game *data, int *x, int *y)
 		{
 			if (ft_strchr("NSEW", data->map.map[*y][*x]))
 			{
-				data->player.player_x = (double)*x + 0.5; // Para que Raycasting funcione correctamente
+				data->player.player_x = (double)*x + 0.5;
 				data->player.player_y = (double)*y + 0.5;
-				data->map.map[*y][*x] = '0'; // borramos el caracter del jugador limpiando el mapa
+				data->map.map[*y][*x] = '0'; // Limpieza visual del mapa
 				return;
 			}
 			(*x)++;
@@ -41,46 +41,31 @@ static int is_valid_position(t_game *data, int x, int y)
 	return (1);
 }
 
+static int is_traversable(char c)
+{
+	// Consideramos como válidos los que pueden recorrerse
+	return (c == '0' || c == ' ' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
+}
+
 static int check_map_borders(t_game *data, char **grid, int x, int y)
 {
 	if (!is_valid_position(data, x, y))
 		return (0);
-	if (grid[y][x] == ' ' || grid[y][x] == '\0')
+	if (grid[y][x] == '\0')
 		return (0);
-	if (grid[y][x] == '1' || grid[y][x] == 'V')
+	if (grid[y][x] == '1' || grid[y][x] == '\t' || grid[y][x] == 'V')
 		return (1);
+	if (!is_traversable(grid[y][x]))
+		return (0);
 
-	grid[y][x] = 'V';
+	grid[y][x] = 'V'; // Visitado
+
 	if (!check_map_borders(data, grid, x + 1, y) ||
 		!check_map_borders(data, grid, x - 1, y) ||
 		!check_map_borders(data, grid, x, y + 1) ||
 		!check_map_borders(data, grid, x, y - 1))
 		return (0);
 
-	return (1);
-}
-
-static int check_internal_spaces(t_game *data)
-{
-	int y;
-	int x;
-
-	y = 0;
-	while (y < data->map.height_map)
-	{
-		x = 0;
-		while (x < (int)ft_strlen(data->map.map[y]))
-		{
-			if (data->map.map[y][x] == ' ')
-			{
-				if (x > 0 && x < (int)ft_strlen(data->map.map[y]) - 1 &&
-					y > 0 && y < data->map.height_map - 1)
-					return (0);
-			}
-			x++;
-		}
-		y++;
-	}
 	return (1);
 }
 
@@ -101,7 +86,7 @@ void parse_validate_map(t_game *data)
 	}
 	valid = check_map_borders(data, temp_grid, x, y);
 	free_grid(temp_grid, data->map.height_map);
-	if (!valid || !check_internal_spaces(data))
+	if (!valid)
 	{
 		report_err(MAP_PLAYABLE_ERR);
 		free_function(data);
