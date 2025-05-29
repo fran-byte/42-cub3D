6f145beb2fd33cd1af6251ed28acded464d9b72f
fran-byte <fran-byte@hotmail.com>
2025-05-29 18:55:38 +0200

    up header

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/04 21:17:52 by frromero          #+#    #+#             */
-/*   Updated: 2025/05/20 20:28:32 by frromero         ###   ########.fr       */
+/*   Created: 2025/04/02 21:17:52 by frromero          #+#    #+#             */
+/*   Updated: 2025/05/29 18:55:16 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,18 @@
  * @param argv Argument vector.
  * @return int Exit status (0 if success, 1 if error).
  */
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	t_game	game;
+    t_game game;
 
-	if (argc != 2)
-		return (report_err(SYNTAX_ERR), 1);
-	init_game(&game);
-	parse_arg(argv[1], &game);
-	init_player(&game);
-	window_init(&game, SCREEN_WIDTH, SCREEN_HEIGHT);
-	render_frame(&game);
-	game_loop(&game);
-	free_function(&game);
-	return (0);
+    if (argc != 2)
+        return (report_err(SYNTAX_ERR), 1);
+    init_game(&game);
+    parse_arg(argv[1], &game);
+    init_player(&game);
+    window_init(&game, SCREEN_WIDTH, SCREEN_HEIGHT);
+    render_frame(&game);
+    game_loop(&game);
+    free_function(&game);
+    return (0);
 }
